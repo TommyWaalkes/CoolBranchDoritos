25bd491606c27632421c45468e756833a5a603af
Tommy Waalkes <Tommy@grandcircus.co>
2020-02-19 21:03:11 -0500

    Added in sick poetry

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -6,6 +6,7 @@ namespace Branching
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Good bye moon");
             Console.WriteLine("Hello World!");
             Console.WriteLine("I want Frito Pie");
             Console.WriteLine("Git is the funnerest thing evears!");
