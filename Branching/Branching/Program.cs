523a00b124a563c915795932d3a571852f57b193
Tommy Waalkes <Tommy@grandcircus.co>
2020-02-19 21:04:41 -0500

    Added in wendys line

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -6,6 +6,7 @@ namespace Branching
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Wendys has the best burgers");
             Console.WriteLine("Hello World!");
             Console.WriteLine("I want Frito Pie");
             Console.WriteLine("Git is the funnerest thing evears!");
